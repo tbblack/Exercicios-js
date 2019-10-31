ae297785791c115d034cf669c580be48a6e0582e
Thales B Oliveira <thalesbarbosadeoliveira@gmail.com>
2019-10-31 18:00:22 -0300

    Atualizando tipagem fraca com comentarios

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,25 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+/*
+A mesma variavel pode alternar
+entre tipos pois a linguagem
+é de tipagem fraca
+a variavel qualquer passou de
+string para number
+*/
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// evitar nomes genéricos e siglas
+
+let valor =''
+let number = 1
+let pqp = false // Produto quimico perigoso... kkk
+                /* usar bons nomes de variaveis para não
+                 * precisar comentar para justificar o nome
+                 */
+
