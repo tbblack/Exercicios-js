d1d34012071b9750794a007ac0de8f05f79773f7
Thales B Oliveira <thalesbarbosadeoliveira@gmail.com>
2019-10-31 13:11:05 -0300

    upando comentario.js

---
+++ fundamentos/comentario.js
@@ -0,0 +1,17 @@
+// Comentario de uma linha
+console.log('Linha 1')
+
+/*
+comentario de
+multiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+ * Comentário de
+ * Multiplas linhas
+ *
+ *
+ *
+ */
+console.log('Linha 3')
