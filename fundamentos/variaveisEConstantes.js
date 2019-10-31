9709f3495deabc1d4d9975a4c4b12111103ffd23
Thales B Oliveira <thalesbarbosadeoliveira@gmail.com>
2019-10-31 13:40:33 -0300

    Adicionando atividade de variaveis e constantes

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,30 @@
+/*
+tente nomear corretamente oque
+esta fazendo para fazer sentido
+e não precisar ficar comentado
+e o codigo ficar limpo
+*/
+
+var a = 3
+let b = 4 //preferencia
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+/*
+Com var é possivel declarar mais
+de uma vez a variavel, ja com let
+não é possivel redeclarar a mesma
+variavel.
+*/
+
+a=300
+b=400
+
+console.log(a, b)
+
+const c=5
+//c = 50 
+console.log(c)
