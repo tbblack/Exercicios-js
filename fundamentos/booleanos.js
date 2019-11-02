14dfa542b01462f71a47448c73e0e2d78e9741b3
Thales B Oliveira <thalesbarbosadeoliveira@gmail.com>
2019-11-02 12:39:36 -0300

    atualizando booleanos

--- fundamentos/booleanos.js
+++ fundamentos/booleanos.js
@@ -20,7 +20,26 @@ console.log('os verdadeiros...')
 console.log(!!3)
 console.log(!!-1)
 console.log(!!' ')
+console.log(!!'texto')
 console.log(!![])
 console.log(!!{})
 console.log(!!Infinity)
 console.log(!!(isAtivo = true))
+console.log(!!(isAtivo = Infinity))
+console.log(!!(isAtivo = 33))
+
+console.log('os falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log('para finalizar..')
+console.log(!!('' || null || 0 || ' '))
+
+let nome = 'Lucas'
+
+console.log(nome || 'Desconhecido')
+// operação boleana, retorna o valor verdadeiro
